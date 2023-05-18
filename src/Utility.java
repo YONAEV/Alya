ca81da07da715cda05da334156943429c8f43bf7
Anastasia_Cydeo <gleasonanastasia@yahoo.com>
2023-05-18 19:43:09 -0400

    added Anastasia's for i loop )))

--- src/Utility.java
+++ src/Utility.java
@@ -4,4 +4,12 @@ public class Utility {
         System.out.println("util1");
     }
 
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            // Anastasia
+
+        }
+    }
+
 }
